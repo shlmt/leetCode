97b707d81354620449c13092ec2cf91f8cb46434
shlmt <155112268+shlmt@users.noreply.github.com>
2024-11-05 15:33:46 +0200

    Time: 0 ms (100%), Space: 44 MB (39.55%) - LeetHub

---
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int sum=1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0)
+                return 0;
+            if(nums[i]<0)
+                sum*=-1;
+        }
+        return sum;
+    }
+}
