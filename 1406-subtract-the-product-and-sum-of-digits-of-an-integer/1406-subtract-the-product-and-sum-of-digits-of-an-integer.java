7ee8c048341991a4081f7b8c0ed0564bb218add4
shlmt <155112268+shlmt@users.noreply.github.com>
2024-11-05 15:29:29 +0200

    Time: 0 ms (100%), Space: 40 MB (88.96%) - LeetHub

---
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/1406-subtract-the-product-and-sum-of-digits-of-an-integer.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int subtractProductAndSum(int n) {
+        int prod=1,sum=0;
+        for(;n!=0;n/=10){
+            prod*=n%10;
+            sum+=n%10;
+        }
+        return prod-sum;
+    }
+}
