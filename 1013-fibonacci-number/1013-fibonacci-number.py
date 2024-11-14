1ea3abdb000cc292cbd3b881c7eb5463e41134cd
shlmt <155112268+shlmt@users.noreply.github.com>
2024-11-14 19:21:11 +0200

    Time: 37 ms (56.61%), Space: 16.5 MB (35.41%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,9 +1,7 @@
-class Solution(object):
-    def fib(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        return n if n in [0,1] else self.fib(n-1)+self.fib(n-2);  
-
-        
+class Solution:
+    def fib(self, n: int) -> int:
+        if n==0: return 0
+        x=y=1
+        for _ in range(n-2):
+            x,y = y,x+y
+        return y
